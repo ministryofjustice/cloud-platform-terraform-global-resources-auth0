e6320f2ab2c8e2f90147359af46e582a37c70929
jakemulley <jakemulley@users.noreply.github.com>
2023-12-29 14:28:23 +0000

    Update syntax to support auth0/auth0 >= 1.0.0

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ resource "auth0_client" "management_auth0_actions" {
   is_first_party       = true
   oidc_conformant      = true
   sso                  = true
-  grant_types = [ "client_credentials" ]
-  
+  grant_types          = ["client_credentials"]
+
 
   jwt_configuration {
     alg                 = "RS256"
@@ -24,29 +24,36 @@ resource "auth0_client" "management_auth0_actions" {
 resource "auth0_client_grant" "management_grant" {
   client_id = auth0_client.management_auth0_actions.id
   audience  = "https://${var.auth0_tenant_domain}/api/v2/"
-  scope     = ["read:user_idp_tokens"]
+  scopes    = ["read:user_idp_tokens"]
+}
+
+# Get client secret
+data "auth0_client" "management_auth0_actions" {
+  client_id = auth0_client.management_auth0_actions.client_id
 }
 
 resource "auth0_action" "add_github_teams_to_oidc_group_claim" {
-    name = "add-github-teams-to-oidc-group-claim"
-    supported_triggers {
-        id = "post-login"
-        version = "v3"
-    }
-    runtime = "node16"
-    code = templatefile("${path.module}/resources/auth0-rules/action-add-github-teams-to-oidc-group-claim.js.tpl", {
-      auth0_tenant_domain = var.auth0_tenant_domain
-      auth0_groupsClaim   = var.auth0_groupsClaim
-      moj_github_org      = var.moj_github_org
-    })
-    secrets {
-        name  = "MGMT_ID"
-        value = auth0_client.management_auth0_actions.client_id
-    }
-    secrets {
-        name  = "MGMT_SECRET"
-        value = auth0_client.management_auth0_actions.client_secret
-    }
+  name    = "add-github-teams-to-oidc-group-claim"
+  runtime = "node16"
+  deploy  = true
+  code = templatefile("${path.module}/resources/auth0-rules/action-add-github-teams-to-oidc-group-claim.js.tpl", {
+    auth0_tenant_domain = var.auth0_tenant_domain
+    auth0_groupsClaim   = var.auth0_groupsClaim
+    moj_github_org      = var.moj_github_org
+  })
 
-    deploy = true
+  supported_triggers {
+    id      = "post-login"
+    version = "v3"
+  }
+
+  secrets {
+    name  = "MGMT_ID"
+    value = auth0_client.management_auth0_actions.client_id
+  }
+
+  secrets {
+    name  = "MGMT_SECRET"
+    value = data.auth0_client.management_auth0_actions.client_secret
+  }
 }
